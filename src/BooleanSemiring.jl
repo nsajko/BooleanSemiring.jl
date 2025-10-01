a0ea7b58cd6dfcc268a195961f29ea4cb4b5353f
Neven Sajko <s@purelymail.com>
2025-10-01 20:30:52 +0200

    init

--- src/BooleanSemiring.jl
+++ src/BooleanSemiring.jl
@@ -1,5 +1,28 @@
 module BooleanSemiring
+    """
+        B
 
-# Write your package code here.
-
+    The two-element Boolean algebra.
+    """
+    struct B <: Integer
+        b::Bool
+        function B(b::Bool)
+            new(b)
+        end
+    end
+    function B(b::Int)
+        B(Bool(b))
+    end
+    function Base.Bool(b::B)
+        b.b
+    end
+    function Base.Int(b::B)
+        Int(Bool(b))
+    end
+    function Base.:(+)(l::B, r::B)
+        B(l.b | r.b)
+    end
+    function Base.:(*)(l::B, r::B)
+        B(l.b & r.b)
+    end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,44 @@
 using BooleanSemiring
+using BooleanSemiring: B
 using Test
-using Aqua
 
 @testset "BooleanSemiring.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(BooleanSemiring)
+    @testset "construction" begin
+        @test B(0) === B(false)
+        @test B(1) === B(true)
     end
-    # Write your tests here.
+    @testset "`zero`, `one`" begin
+        @test zero(B) === B(0)
+        @test one(B) === B(1)
+        @test zero(B(1)) === B(0)
+        @test one(B(1)) === B(1)
+    end
+    @testset "`iszero``, `isone`" begin
+        @test iszero(B(0))
+        @test isone(B(1))
+    end
+    @testset "matrix multiplication" begin
+        @testset "2x2" begin
+            mat_one = [B(1) B(0); B(0) B(1)]
+            mat_zero = [B(0) B(0); B(0) B(0)]
+            enumer = (B(0), B(1))
+            for (b11, b12, b21, b22) ∈ Iterators.product(enumer, enumer, enumer, enumer)
+                mat = [b11 b12; b21 b22]
+                @test mat_zero == mat * mat_zero
+                @test mat_zero == mat_zero * mat
+                @test mat_zero == mat .* mat_zero
+                @test mat_zero == mat_zero .* mat
+                @test mat == mat * mat_one
+                @test mat == mat_one * mat
+                @test mat_one == mat_one + mat_one
+                @test mat_one == mat_one .+ mat_one
+            end
+        end
+    end
+end
+
+using Aqua: Aqua
+
+@testset "Code quality (Aqua.jl)" begin
+    Aqua.test_all(BooleanSemiring)
 end
